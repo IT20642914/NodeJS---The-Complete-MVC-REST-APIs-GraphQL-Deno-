1df0b116926a10b4e9d57f931340d72a30859991
Avishka <avishkachanaka@gmail.com>
2024-06-26 16:01:37 +0530

    chore: Update database configuration and start server

--- NodeJsCompleteGuide/03-getting-a-single-product/03-getting-a-single-product/app.js
+++ NodeJsCompleteGuide/03-getting-a-single-product/03-getting-a-single-product/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database')
 const sequelize = require('./util/database');
 
 const app = express();
@@ -14,7 +13,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// const sequelize = require('sequelize');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,14 +20,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
 app.use(errorController.get404);
-// Initialize database and start server
-sequelize.sync().then(result => {
-    console.log('Database sync successful:', result);
-    app.listen(3000, () => console.log('Server running on port 3000'));
-}).catch(err => {
-    console.error("Database error:", err);
-});
 
-// app.listen(3000);
+sequelize
+  .sync()
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
